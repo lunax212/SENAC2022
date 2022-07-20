4f0b60df573eb1b7d393eba3f3cd6d9b1186396d
Bianca <79856930+lunax212@users.noreply.github.com>
2022-07-20 14:59:21 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,58 @@
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon">
+    <title>Eletrotec</title>
+    
+    <link rel="stylesheet" href="css/estilo.css" type="text/css" media="screen" />
+    
+</head>
+<body>
+
+  <section class="container">
+
+   <header class="topo">
+
+    <div class="logo"><img src="imagens/logo.png" width="233" height="75">
+
+    </div><!--Fim logo-->
+
+
+    <nav class="menu">
+      <ul>
+          <li> <a href="home.php">Home</a> </li>
+          <li> <a href="quemsomos.html">LojaTec</a> </li>
+          <li> <a href="produtos.html">Fotos</a> </li>
+          <li> <a href="cliente.html">Acessórios</a> </li>
+          <li> <a href="contato.html">Camêras</a> </li>
+          <li> <a href="contato.html">Contato</a> </li>
+      </ul>
+      <p class="contato"> (21) xxxx-xxxx |(21) xxxx-xxxx |(21) xxxx-xxxx |(21) xxxx-xxxx  </p>
+
+
+    </nav><!--Fim menu-->
+
+  
+
+  </header><!--Fim topo-->
+</section>
+
+
+       <div class="slide first">    
+          <img src="imagens/a8f0e55446126a6a24f2_Banner PCNITRO.jpg" width="1300" height="500"/>
+       </div>
+    
+
+
+</body>
+
+
+</html>
+
+
+
